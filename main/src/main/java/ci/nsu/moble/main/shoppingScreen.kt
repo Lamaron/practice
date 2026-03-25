ee565700b8ba875e44f08ae9a25810f46a5262f8
Alkon <sasha12345333.ru@bk.ru>
2026-03-25 18:26:57 +0700

    Создал нужные файлы MainActivity.kt shoppingScreen.kt ShoppingViewModel. и заполнил их шаблонами из task 4

---
+++ main/src/main/java/ci/nsu/moble/main/shoppingScreen.kt
@@ -0,0 +1,10 @@
+package ci.nsu.moble.main
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ShoppingScreen(){
+
+}
