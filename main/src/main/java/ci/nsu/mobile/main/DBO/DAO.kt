e652d063a2dd320d1aeb91055729b4477d538bd9
Alkon <sasha12345333.ru@bk.ru>
2026-04-09 10:07:56 +0700

    Data Access Object (DAO)

--- main/src/main/java/ci/nsu/mobile/main/DBO/DAO.kt
+++ main/src/main/java/ci/nsu/mobile/main/DBO/DAO.kt
@@ -5,6 +5,7 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
+
 @Dao
 interface DepositDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
