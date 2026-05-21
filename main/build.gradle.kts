fa58a5710e0149010f3c74287f2dade57c9248e1
Alkon <sasha12345333.ru@bk.ru>
2026-05-21 11:41:43 +0700

    added dependencies

--- main/build.gradle.kts
+++ main/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     alias(libs.plugins.kotlin.compose)
+    id("org.jetbrains.kotlin.plugin.serialization")
 }
 
 android {
